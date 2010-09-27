83c021c475a86a0357505f5448f85e88fd02706d
kderlin <kderlin@920f35b2-2537-e172-05d8-414e666e66da>
2010-09-27 04:24:07 +0000

    完善项目信息和用法说明

--- trunk/pack.bat
+++ trunk/pack.bat
@@ -1,2 +1,5 @@
 @cd /d %~p0
-setup.py py2exe && upx dist/lzu_net_auth.exe
+C:\Python27\python.exe setup.py py2exe && upx dist/lzu_net_auth.exe
+move dist\lzu_net_auth.exe .
+7z a lzunet-1.0.1-win.7z lzu_net_auth.exe µÇÂ¼.bat ÏÂÏß.bat
+@pause
