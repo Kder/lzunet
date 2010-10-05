6f167e612da78c12be0fd548bb55117dd8aa470e
kderlin <kderlin@920f35b2-2537-e172-05d8-414e666e66da>
2010-10-05 12:36:38 +0000

    a new feature: show local ip address when operation completed.

--- lzu_net_auth.py
+++ lzu_net_auth.py
@@ -79,34 +79,118 @@ def con_auth(ul, bd, rf, tu):
         print(u'已下线 Logout successfully.')
     return 0
 
-if len(sys.argv) == 2: #For logout
-    if sys.argv[1] == 'logout':
-        url = 'http://1.1.1.1/userout.magi'
-        body = (('imageField', 'logout'),('userout','logout'))
-        referer = ('Referer', 'http://1.1.1.1/logout.htm')
-elif len(sys.argv) == 3: #For login
-    url = 'http://1.1.1.1/passwd.magi'
-    body = (
-    ('userid',sys.argv[1]),
-    ('passwd',sys.argv[2]),
-    ('serivce','internet'),
-    ('chap','0'),
-    ('random','internet'),
-    )
-    referer = ('Referer', 'http://1.1.1.1/')
-else:
-    print(__doc__)
-    sys.exit(3)
+#Get the IP address of local machine
+#code from: 
+# http://hi.baidu.com/yangyingchao/blog/item/8d26b544f6059f45500ffe78.html
 
-test_url = 'http://www.baidu.com/'
-#fenc = sys.getfilesystemencoding()
+# for Linux
+def get_ip_address(ifname):
+    import socket
+    import fcntl
+    import struct
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
-try:
-    if con_auth(url, body, referer, test_url) == 0:
-        print(u'操作完成 OK')
-except:
-    print(u'发生错误，请稍后再试 Error occured. Please try again later.')
-#finally:
-#    raw_input('请按回车键退出 Press Return to quit...'.decode('utf-8').encode(fenc))
+#get_ip_address('lo')
+#get_ip_address('eth0')
+
+# for Windows
+def getIPAddresses():
+    from ctypes import Structure, windll, sizeof
+    from ctypes import POINTER, byref
+    from ctypes import c_ulong, c_uint, c_ubyte, c_char
+    MAX_ADAPTER_DESCRIPTION_LENGTH = 128
+    MAX_ADAPTER_NAME_LENGTH = 256
+    MAX_ADAPTER_ADDRESS_LENGTH = 8
+    class IP_ADDR_STRING(Structure):
+        pass
+    LP_IP_ADDR_STRING = POINTER(IP_ADDR_STRING)
+    IP_ADDR_STRING._fields_ = [
+        ("next", LP_IP_ADDR_STRING),
+        ("ipAddress", c_char * 16),
+        ("ipMask", c_char * 16),
+        ("context", c_ulong)]
+    class IP_ADAPTER_INFO (Structure):
+        pass
+    LP_IP_ADAPTER_INFO = POINTER(IP_ADAPTER_INFO)
+    IP_ADAPTER_INFO._fields_ = [
+        ("next", LP_IP_ADAPTER_INFO),
+        ("comboIndex", c_ulong),
+        ("adapterName", c_char * (MAX_ADAPTER_NAME_LENGTH + 4)),
+        ("description", c_char * (MAX_ADAPTER_DESCRIPTION_LENGTH + 4)),
+        ("addressLength", c_uint),
+        ("address", c_ubyte * MAX_ADAPTER_ADDRESS_LENGTH),
+        ("index", c_ulong),
+        ("type", c_uint),
+        ("dhcpEnabled", c_uint),
+        ("currentIpAddress", LP_IP_ADDR_STRING),
+        ("ipAddressList", IP_ADDR_STRING),
+        ("gatewayList", IP_ADDR_STRING),
+        ("dhcpServer", IP_ADDR_STRING),
+        ("haveWins", c_uint),
+        ("primaryWinsServer", IP_ADDR_STRING),
+        ("secondaryWinsServer", IP_ADDR_STRING),
+        ("leaseObtained", c_ulong),
+        ("leaseExpires", c_ulong)]
+    GetAdaptersInfo = windll.iphlpapi.GetAdaptersInfo
+    GetAdaptersInfo.restype = c_ulong
+    GetAdaptersInfo.argtypes = [LP_IP_ADAPTER_INFO, POINTER(c_ulong)]
+    adapterList = (IP_ADAPTER_INFO * 10)()
+    buflen = c_ulong(sizeof(adapterList))
+    rc = GetAdaptersInfo(byref(adapterList[0]), byref(buflen))
+    if rc == 0:
+        for a in adapterList:
+            adNode = a.ipAddressList
+            while True:
+                ipAddr = adNode.ipAddress
+                if ipAddr:
+                    yield ipAddr
+                adNode = adNode.next
+                if not adNode:
+                    break
+
+def get_ip():
+    if sys.platform == 'win32':
+        return [x for x in getIPAddresses()]
+    else:
+        return get_ip_address('eth0')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2: #For logout
+        if sys.argv[1] == 'logout':
+            url = 'http://1.1.1.1/userout.magi'
+            body = (('imageField', 'logout'),('userout','logout'))
+            referer = ('Referer', 'http://1.1.1.1/logout.htm')
+    elif len(sys.argv) == 3: #For login
+        url = 'http://1.1.1.1/passwd.magi'
+        body = (
+        ('userid',sys.argv[1]),
+        ('passwd',sys.argv[2]),
+        ('serivce','internet'),
+        ('chap','0'),
+        ('random','internet'),
+        )
+        referer = ('Referer', 'http://1.1.1.1/')
+    else:
+        print(__doc__)
+        sys.exit(3)
+
+    test_url = 'http://www.baidu.com/'
+    #fenc = sys.getfilesystemencoding()
+
+    try:
+        if con_auth(url, body, referer, test_url) == 0:
+            print('Your IP: ' + str(get_ip()))
+            print(u'操作完成 OK')
+    except:
+        print(u'发生错误，请稍后再试 Error occured. Please try again later.')
+    #finally:
+    #    raw_input('请按回车键退出 Press Return to quit...'.decode('utf-8').encode(fenc))
+    
 
 #vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
