f1eb69154e567e5357cc20e15fee1283c9ef73aa
kderlin <kderlin@920f35b2-2537-e172-05d8-414e666e66da>
2010-10-24 07:21:27 +0000

    按PEP8风格规范代码

--- lzunet3.py
+++ lzunet3.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-'''兰大上网认证系统自动登录工具。可以实现一键登录/一键下线，无需打开浏览器，无需再手动输入邮箱和密码。 
+'''兰大上网认证系统自动登录工具。可以实现一键登录/一键下线，无需打开浏览器，
+无需再手动输入邮箱和密码。
 
 用法：
 
-    把下载到的压缩包中的“登录.bat”中的“邮箱 密码”替换为你的邮箱和上网认证密码，保存，然后双击 登录.bat 即可登录。"下线.bat"不必更改，要下线直接双击就行。 
+    把下载到的压缩包中的“登录.bat”中的“邮箱 密码”替换为你的邮箱和上网认证密码，保存，
+    然后双击 登录.bat 即可登录。"下线.bat"不必更改，要下线直接双击就行。
 
-    Linux用户请svn checkout源代码，设置好connect.sh中对应的mail和pass，运行connect.sh即可登录，logout.sh不要修改，直接运行就可以下线。 
-    
+    Linux用户请svn checkout源代码，设置好connect.sh中对应的mail和pass，
+    运行connect.sh即可登录，logout.sh不要修改，直接运行就可以下线。
+
     要直接使用lzu_net_auth，命令格式为：
         登录：
             lzu_net_auth 邮箱 密码
         退出：
             lzu_net_auth logout
-            
+
 '''
 
-__author__= 'Kder'
+__author__ = 'Kder'
 __copyright__ = 'Copyright 2010 Kder'
 __credits__ = ['Kder']
 __maintainer__ = "Kder"
@@ -30,10 +33,11 @@ __projecturl__ = 'http://code.google.com/p/lzunet/'
 __version__ = '1.1.0'
 __revision__ = "$Revision$"
 __date__ = '$Date$'
-__author__= '$Author$'
+__author__ = '$Author$'
 
 
-import sys,os
+import sys
+import os
 import urllib
 import urllib.request
 import urllib.parse
@@ -44,14 +48,19 @@ def con_auth(ul, bd, rf, tu):
     cj = http.cookiejar.CookieJar()
     op = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
     if sys.platform == 'win32':
-        op.addheaders = [('User-Agent','Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'),
-                         ('Accept','application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5'),
+        op.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows; U; \
+Windows NT 5.1; en-US) AppleWebKit/534.3 (KHTML, like Gecko) \
+Chrome/6.0.472.63 Safari/534.3'),
+        ('Accept', 'application/xml, application/xhtml+xml, \
+text/html;q=0.9, text/plain;q=0.8, image/png,*/*;q=0.5'),
                          rf]
     else:
-        op.addheaders = [('User-Agent','Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.2.10) Gecko/20100916 Firefox/3.6.10'),
-                         ('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'), rf]
+        op.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; U; Linux i686; \
+en-US; rv:1.9.2.10) Gecko/20100916 Firefox/3.6.10'),
+                         ('Accept', 'text/html, application/xhtml+xml, \
+application/xml;q=0.9,*/*;q=0.8'), rf]
     urllib.request.install_opener(op)
-    req = urllib.request.Request(ul,urllib.parse.urlencode(bd))
+    req = urllib.request.Request(ul, urllib.parse.urlencode(bd))
     u = urllib.request.urlopen(req)
     ret = u.read().decode('gb2312')
     if os.getenv('LNA_DEBUG'):
@@ -63,16 +72,18 @@ def con_auth(ul, bd, rf, tu):
         print('帐号欠费，测试期间可携带校园卡来网络中心办理。 ')
         return 5
     elif '范围' in ret:
-        print('在线用户超出允许的范围：帐号已在别处登录，如果确认不是自己登录的，可以联系网络中心踢对方下线。')
+        print('在线用户超出允许的范围：帐号已在别处登录，如果确认不是自己登录的，\
+可以联系网络中心踢对方下线。')
         return 4
     elif 'Timeout' in ret:
         try:
-            f = urllib.request.urlopen(tu).read(21)
+            f = urllib.request.urlopen(tu)
+            r = f.read(21)
             if not ('PUBLIC' in f):
                 print('已连接 Connected')
                 return 0
-        except:
-            print('验证超时(不影响正常上网，请打开浏览器刷新页面即可) Timeout')
+        except Exception as e:
+            print('验证超时(不影响正常上网，请打开浏览器刷新页面即可) Timeout', e)
         return 2
     elif 'Password error' in ret:
         print('用户名或密码错误 Username or Password error')
@@ -86,8 +97,9 @@ def con_auth(ul, bd, rf, tu):
     return 0
 
 #Get the IP address of local machine
-#code from: 
+#code from:
 # http://hi.baidu.com/yangyingchao/blog/item/8d26b544f6059f45500ffe78.html
+
 
 # for Linux
 def get_ip_address(ifname):
@@ -104,6 +116,7 @@ def get_ip_address(ifname):
 #get_ip_address('lo')
 #get_ip_address('eth0')
 
+
 # for Windows
 def getIPAddresses():
     from ctypes import Structure, windll, sizeof
@@ -112,6 +125,7 @@ def getIPAddresses():
     MAX_ADAPTER_DESCRIPTION_LENGTH = 128
     MAX_ADAPTER_NAME_LENGTH = 256
     MAX_ADAPTER_ADDRESS_LENGTH = 8
+
     class IP_ADDR_STRING(Structure):
         pass
     LP_IP_ADDR_STRING = POINTER(IP_ADDR_STRING)
@@ -120,6 +134,7 @@ def getIPAddresses():
         ("ipAddress", c_char * 16),
         ("ipMask", c_char * 16),
         ("context", c_ulong)]
+
     class IP_ADAPTER_INFO (Structure):
         pass
     LP_IP_ADAPTER_INFO = POINTER(IP_ADAPTER_INFO)
@@ -154,7 +169,7 @@ def getIPAddresses():
             ipAddr = adNode.ipAddress
             if ipAddr:
                 yield ipAddr
-            
+
 #            while True:
 #                ipAddr = adNode.ipAddress
 #                if ipAddr:
@@ -162,6 +177,7 @@ def getIPAddresses():
 #                adNode = adNode.__next__
 #                if not adNode:
 #                    break
+
 
 def get_ip():
     if sys.platform == 'win32':
@@ -172,20 +188,20 @@ def get_ip():
 
 if __name__ == '__main__':
     #logout
-    if len(sys.argv) == 2: 
+    if len(sys.argv) == 2:
         if sys.argv[1] == 'logout':
             url = 'http://1.1.1.1/userout.magi'
-            body = (('imageField', 'logout'),('userout','logout'))
+            body = (('imageField', 'logout'), ('userout', 'logout'))
             referer = ('Referer', 'http://1.1.1.1/logout.htm')
     #login
-    elif len(sys.argv) == 3: 
+    elif len(sys.argv) == 3:
         url = 'http://1.1.1.1/passwd.magi'
         body = (
-        ('userid',sys.argv[1]),
-        ('passwd',sys.argv[2]),
-        ('serivce','internet'),
-        ('chap','0'),
-        ('random','internet'),
+        ('userid', sys.argv[1]),
+        ('passwd', sys.argv[2]),
+        ('serivce', 'internet'),
+        ('chap', '0'),
+        ('random', 'internet'),
         )
         referer = ('Referer', 'http://1.1.1.1/')
     else:
@@ -203,7 +219,8 @@ if __name__ == '__main__':
         print(e)
         print('发生错误，请稍后再试 Error occured. Please try again later.')
     #finally:
-    #    raw_input('请按回车键退出 Press Return to quit...'.decode('utf-8').encode(fenc))
-    
+    #    raw_input('''请按回车键退出 Press Return to quit...
+#    '''.decode('utf-8').encode(fenc))
+
 
 #vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
