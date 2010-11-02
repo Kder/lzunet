2a5c8969b222aa64776067eacd09a753461027e6
kderlin <kderlin@920f35b2-2537-e172-05d8-414e666e66da>
2010-11-02 14:46:24 +0000

    upx压缩dll和exe文件

--- pack.bat
+++ pack.bat
@@ -2,4 +2,5 @@
 F:\Python27\python.exe setup.py py2exe && upx dist/lzunet.exe
 move dist\lzunet.exe .
 7z a lzunet-1.1.0-win.7z lzunet.exe µÇÂ¼.bat ÏÂÏß.bat
+7z a -t7z -xr!*.svn* lzunet-lua-1.0-win.7z lzunet-lua
 @pause
