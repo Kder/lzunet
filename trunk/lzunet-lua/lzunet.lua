0213a4aefe14127f9a7a37e1c70c1cbf450dddbb
kderlin <kderlin@920f35b2-2537-e172-05d8-414e666e66da>
2010-11-06 14:19:22 +0000

    增加显示本机IP功能

--- trunk/lzunet-lua/lzunet.lua
+++ trunk/lzunet-lua/lzunet.lua
@@ -1,6 +1,9 @@
---~ socket = require("socket")
+﻿--~ lzunet.lua
+--~ lzunet lua version for *nix utf-8 environment
+--~ by Kder
+--~ license: GPLv3
+
 http = require("socket.http")
---~ urllib = require("socket.url")
 
 
 function con_auth(ul, bd, rf, tu)
@@ -26,6 +29,11 @@ function con_auth(ul, bd, rf, tu)
 --~     ret = table.concat(res,'\n')
     ret = table.concat(response_body)
 
+	if ret == '' then
+		print('请检查网络是否连接正常\n')
+		return 1
+	end
+
     if os.getenv('LNA_DEBUG') then
         print(ret)
     end
@@ -42,16 +50,14 @@ function con_auth(ul, bd, rf, tu)
 可以联系网络中心踢对方下线。')
         return 4
     elseif string.find(ret, 'Timeout') then
---~         try:
-            f = http.request(tu)
---~             print(f)
-            if string.find(f, '百度') then
-                print('已连接 Connected')
-                return 0
-            end
---~         except:
---~             print('验证超时(不影响正常上网，请打开浏览器刷新页面即可) Timeout')
---~         return 3
+        f = http.request(tu)
+
+        if string.find(f, '百度') then
+            print('已连接 Connected')
+--~         print('验证超时(不影响正常上网，请打开浏览器刷新页面即可) Timeout')
+            return 0
+        end
+
     elseif string.find(ret, 'Password error') then
         print('用户名或密码错误 Username or Password error')
         return 2
@@ -68,14 +74,49 @@ function con_auth(ul, bd, rf, tu)
     return 0
 end
 
---~ #logout
+
+
+function getLocalIP()
+    if os.getenv("OS")=='Windows_NT' then
+        require('luacom')
+        computer = "."
+        oWMIService = luacom.GetObject ("winmgmts:{impersonationLevel=Impersonate}!\\\\" ..computer.. "\\root\\cimv2")
+        oRefresher = luacom.CreateObject ("WbemScripting.SWbemRefresher")
+        refobjProcessor = oRefresher:AddEnum(oWMIService,"Win32_PerfFormattedData_PerfOS_Processor").ObjectSet
+        IPConfigSet = oWMIService:ExecQuery("Select IPAddress from Win32_NetworkAdapterConfiguration ")
+        oRefresher:Refresh ()
+
+        for index,item in luacomE.pairs (IPConfigSet) do
+            if item.IPAddress(index-1) ~= nil then
+                ip = item:IPAddress(index-1)
+        --~ 		print(ip,#ip)
+                return ip
+            end
+        end
+
+    else
+--~ 	the following code(8 lines) is from irserversb project (http://code.google.com/p/irserversb/)
+        local ipaddr
+        local cmd = io.popen("/sbin/ifconfig eth0")
+        for line in cmd:lines() do
+                ipaddr = string.match(line, "inet addr:([%d%.]+)")
+                if ipaddr ~= nil then break end
+        end
+        cmd:close()
+        return ipaddr or "?.?.?.?"
+    end
+
+end
+
+
+--~ logout
 if #arg == 1 then
     if arg[1] == 'logout' then
         url = 'http://1.1.1.1/userout.magi'
         body = 'imageField=logout&userout=logout'
         referer = 'http://1.1.1.1/logout.htm'
     end
---~ #login
+--~ login
 elseif #arg == 2 then
     url = 'http://1.1.1.1/passwd.magi'
     body = 'userid='..arg[1]..'&passwd='..arg[2]..'&serivce=internet&chap=0&random=internet'
@@ -91,18 +132,17 @@ else
 end
 
 test_url = 'http://www.baidu.com/'
---~ #fenc = sys.getfilesystemencoding()
+
+
+local ip = getLocalIP()
+print('Your IP: '..ip)
+
+
 if con_auth(url, body, referer, test_url) == 0 then
     print('操作完成 OK')
 else
     print('发生错误，请稍后再试 Error occured. Please try again later.')
 end
---~ try:
---~     if con_auth(url, body, referer, test_url) == 0:
---~         print('Your IP: ' + str(get_ip()))
---~         print(u'操作完成 OK')
---~ except Exception as e:
---~     print(e)
 
 
 --~ $Id$
