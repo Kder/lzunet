68c2d8b4f217fe74f2fa057e6ac300fc320e1c11
kderlin <kderlin@920f35b2-2537-e172-05d8-414e666e66da>
2010-12-26 05:11:06 +0000

    auto get ip address using arp command

--- trunk/lzunet-bat/下线.bat
+++ trunk/lzunet-bat/下线.bat
@@ -1,7 +1,7 @@
 @echo off
 MODE CON: COLS=50 LINES=20
 TITLE lzunet-bat 兰大上网认证系统登录程序
-set IP=219.246.62.167
+for /f "tokens=1,2 delims= " %%i in ('arp.exe -a ^| find "Interface"') do set IP=%%j
 echo\
 color 79
 for /f "tokens=*" %%i in ('curl --silent --data "wlanuserip=%IP%&wlanacname=BAS_138&wlanacIp=202.201.1.138&portalUrl=''&usertime=3146400&imageField=''" --referer http://202.201.1.140/portalAuthAction.do  --user-agent "Mozilla/5.0 (Windows NT 5.1; rv:2.0b8) Gecko/20100101 Firefox/4.0b8"  "http://202.201.1.140/portalDisconnAction.do" ^| findstr /r "alert(.*)"') do set ss=%%i 
