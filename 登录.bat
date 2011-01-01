f2d6c42575fdc05ad5c3ed5cf7aec500557f28e7
kderlin <kderlin@920f35b2-2537-e172-05d8-414e666e66da>
2011-01-01 04:17:14 +0000

    更新用法说明，发布lzunet1.1.1版

--- 登录.bat
+++ 登录.bat
@@ -1,6 +1,7 @@
 @echo off
 set PATHEXT=%PATHEXT%;.py;.pyw
 cd /d %~p0
-for /f "tokens=1,2* delims= " %%i in (lzunet.txt) do set userid=%%i&&set passwd=%%j
-startup.py %userid% %passwd%
+rem for /f "tokens=1,2* delims= " %%i in (lzunet.txt) do set userid=%%i&&set passwd=%%j
+rem startup.py %userid% %passwd%
+startup.py login
 pause
