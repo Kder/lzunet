ccd8b6a2858eea839e47c74c794dcb1c4e0e38d1
kderlin <kderlin@920f35b2-2537-e172-05d8-414e666e66da>
2011-01-10 12:14:01 +0000

    update linux start scripts

--- trunk/pack.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-7z a lzunet-1.2.0.68-src.7z lzunet.py lzunet3.py startup.py lzunet.txt login.sh logout.sh µÇÂ¼.bat ÏÂÏß.bat
-rem 7z a -tzip -xr!*.svn* lzunet-lua-1.2.3.zip lzunet-lua
-pause
