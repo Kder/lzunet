9a5fa452302c91bcb76e6af5f1e211add0fe2459
kderlin <kderlin@920f35b2-2537-e172-05d8-414e666e66da>
2011-01-10 00:47:15 +0000

    发布1.2.0版本

--- 登录.bat
+++ 登录.bat
@@ -3,8 +3,8 @@ set PATHEXT=%PATHEXT%;.py;.pyw
 cd /d %~dp0
 REM for /f "tokens=1,2* delims= " %%i in (lzunet.txt) do set userid=%%i&&set passwd=%%j
 if not exist startup.py (
-    lzunet
+    .\lzunet
 ) else (
-    startup
+    .\startup
 )
 pause
