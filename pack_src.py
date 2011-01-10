0d03aeb2cee349fbfc97c16324aa301c596c0576
kderlin <kderlin@920f35b2-2537-e172-05d8-414e666e66da>
2011-01-10 12:27:28 +0000

    improved pack_src

--- pack_src.py
+++ pack_src.py
@@ -1,7 +1,14 @@
 #coding:utf8
 import os
 import sys
+import lzunet
+
+
+def checkdigit(x):
+  if x.isdigit():
+    return x
+
+rev = filter(checkdigit, lzunet.__revision__)
+
 fenc = sys.getfilesystemencoding()
-os.system(u'7z a lzunet-1.2.0.69-src.7z lzunet.py lzunet3.py startup.py lzunet.txt login.sh logout.sh 登录.bat 下线.bat'.encode(fenc))
-
-
+os.system(u'7z a lzunet-%s.%s-src.7z lzunet.py lzunet3.py startup.py lzunet.txt login.sh logout.sh 登录.bat 下线.bat'.encode(fenc) % (lzunet.__version__, rev))
