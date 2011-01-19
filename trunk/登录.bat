7cbdb2ea1d2d349fd27da350b514e0591cb66303
kderlin@gmail.com <kderlin@gmail.com@920f35b2-2537-e172-05d8-414e666e66da>
2011-01-19 11:18:21 +0000

    更新Windows登录脚本

--- trunk/登录.bat
+++ trunk/登录.bat
@@ -1,6 +1,5 @@
 @echo off
-set PATHEXT=%PATHEXT%;.py;.pyw
+set PATHEXT=%PATHEXT%;.py
 cd /d %~dp0
-REM for /f "tokens=1,2* delims= " %%i in (lzunet.txt) do set userid=%%i&&set passwd=%%j
 .\lzunet
 pause
